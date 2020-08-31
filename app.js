00e0cce184f22a2be13fad0ac7a3f3909d1a33f5
Waldman200036 <Waldman200036@gmail.com>
2020-08-30 21:50:09 -0500

    modified:   app.js 	modified:   src/routes/authRoutes.js

--- app.js
+++ app.js
@@ -20,11 +20,43 @@ app.use(bodyParser.json());
 
 app.use('/auth', authRouter);
 
-const insertDocument = function(db, doc, col, callback) {
+const updateDocument = function (db, doc, callback) {
+  // Get the documents collection
+  const collection = db.collection(doc);
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({
+    email: doc.email
+  }, {
+    $set: {
+      firstname: doc.firstname,
+      lastname: doc.lastname,
+      Organization: doc.Organization,
+      branch: doc.branch,
+      facility: doc.facility,
+      phone: doc.phone,
+      date_select_0: doc.date_select_0,
+      time_select_0: doc.time_select_0,
+      date_select_1: doc.date_select_1,
+      time_select_1: doc.time_select_1,
+      date_select_2: doc.date_select_2,
+      time_select_2: doc.time_select_2,
+      date_select_3: doc.date_select_3,
+      time_select_3: doc.time_select_3,
+      timestamp: doc.timestamp
+    }
+  }, function (err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    debug("Updated the document with the email a equal to 2");
+    callback(result);
+  });
+};
+
+const insertDocument = function (db, doc, col, callback) {
   // Get the documents collection
   const collection = db.collection(col);
   // Insert some documents
-  collection.insertOne(doc, function(err, result) {
+  collection.insertOne(doc, function (err, result) {
     assert.equal(err, null);
     assert.equal(1, result.result.n);
     assert.equal(1, result.ops.length);
@@ -32,76 +64,81 @@ const insertDocument = function(db, doc, col, callback) {
     debug(result);
     callback(result);
   });
-}
+};
 
-const findDocuments = function(db,email, callback) {
+const findDocuments = function (db, email, callback) {
   // Get the documents collection
   const collection = db.collection('trainee');
   // Find some documents
-  collection.find({email: email}).toArray(function(err, docs) {
+  collection.find({
+    email: email
+  }).toArray(function (err, docs) {
     assert.equal(err, null);
     debug("Found the following records");
     // debug(docs);
     callback(docs);
   });
-}
+};
 app.get('/', (req, resp) => {
   console.log('Welcome to my API!!!');
   resp.send('Welcome to my API!!!');
 });
 
 app.get('/apiTraining/get/:email', (req, res) => {
-  
+
   // Local Connection URL
-   const url = 'mongodb://localhost:27017';
+  const url = 'mongodb://localhost:27017';
   // Database Name
   const dbName = 'Training';
   // Use connect method to connect to the server
-       MongoClient.connect(url, function(err, client) {
-       
-      assert.equal(null, err);
-      debug("Connected successfully to server");
+  MongoClient.connect(url, function (err, client) {
 
-      const db = client.db(dbName); 
+    assert.equal(null, err);
+    debug("Connected successfully to server");
 
-        //      return res.json({
-        //   message: 'Handling GET request to /apiTraining/get/email:/',
-        //   body: req.params.email
-        // });       
+    const db = client.db(dbName);
 
-      findDocuments(db,req.params.email, function(results){
-       return res.json({
-          message: 'Handling GET request to /apiTraining/get/email:/',
-          body: results
-        });        
-        client.close();
+    //      return res.json({
+    //   message: 'Handling GET request to /apiTraining/get/email:/',
+    //   body: req.params.email
+    // });       
+
+    findDocuments(db, req.params.email, function (results) {
+      client.close();    
+      return res.json({
+        message: 'Handling GET request to /apiTraining/get/email:/',
+        body: results
       });
-      
+
     });
+
+  });
 });
 app.post('/apiTraining/post', (req, res) => {
-  
+
   // Local Connection URL
-   const url = 'mongodb://localhost:27017';
+  // const url = 'mongodb://localhost:27017';
+  const url = 'mongodb+srv://dbAPIUser:aAzzhulCjjjw1a6R@cluster0.1mk1d.mongodb.net/Training?retryWrites=true&w=majority';
   // Database Name
   const dbName = 'Training';
   // Use connect method to connect to the server
-       MongoClient.connect(url, function(err, client) {
-       
-      assert.equal(null, err);
-      debug("Connected successfully to server");
+  MongoClient.connect(url, function (err, client) {
 
-      const db = client.db(dbName); 
+    assert.equal(null, err);
+    debug("Connected successfully to server");
 
-      insertDocument(db,req.body,'trainee', function(results){
-       return res.json({
-          message: 'Handling GET request to /Trainees',
-          body: results
-        });        
-        client.close();
+    const db = client.db(dbName);
+
+    insertDocument(db, req.body, 'trainee', function (results) {
+      client.close();
+      return res.json({
+        message: 'Handling GET request to /Trainees',
+        body: results
       });
-      
+
     });
+
+  });
 });
 
 app.post('/auth', (req, resp) => {
